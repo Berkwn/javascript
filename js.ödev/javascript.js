9218eee815a9f39363c23d0c9af1a42af46ca1f1
Berkwn <berkand180@gmail.com>
2023-11-05 19:45:20 +0300

    Javascript_Odev
    JS

---
+++ js.ödev/javascript.js
@@ -0,0 +1,134 @@
+//1. soru 
+
+/*let ayGir=prompt("lütfen ay giriniz")
+
+let yaz=["haziran","temmuz","ağustos"]
+let sonbahar=["eylül","ekim","kasım"]
+let kıs=["aralık","ocak","şubat"]
+let ilkbahar=["mart","nisan","mayıs"]
+
+if(yaz.includes(ayGir)){
+
+    alert("sen yaz mevsimindesin")
+}else if(sonbahar.includes(ayGir)){
+
+    alert("sen sonbahardasın")
+}else if(kıs.includes(ayGir))
+{
+    alert("sen kış ayındasın")
+}else if (ilkbahar.includes(ayGir)){
+
+    alert8("sen ilkbahar ayındasın")
+}else{
+    alert("lütfen düzgün bir ay gir")
+}*/
+
+
+
+//2.soru
+
+/*let sayiGir=prompt("lütfen bir sayı giriniz")
+if(sayiGir%2==0){
+    alert("çift sayıdır")
+}else(
+    alert("tek sayıdır")
+)*/
+
+
+
+
+
+
+//3.soru
+
+/*let ad=prompt("adınızı girin")
+
+let yas=prompt("yaşınızı girin..")
+
+if(ad.length<=3){
+
+    alert("böyle bir isim kullanamazsınız")
+}else{
+
+    alert(`adınız: ${ad}`)
+
+}
+
+if(Number.yas<0){
+
+    alert("böyler bir yaş girilemez")
+}else{
+
+
+    alert(`yaşınız: ${yas}`)
+}*/
+
+//4.soru
+
+/*let sayi1=prompt("sayı1")
+let sayi2=prompt("sayi2")
+
+if(sayi1==sayi2){
+
+    alert("sayılar eşit")
+}else if(sayi1>=sayi2){
+
+    alert("say1 büyüktür sayi2'den")
+}else if(sayi1<=sayi2){
+    alert("sayı1 küçüktür sayi2'den")
+}
+
+*/
+
+//5.soru
+
+
+/*let kullanıcıAd=["ömer","ali","hasan"]
+
+let kullanıcıAdGir=prompt("kullanıcı adı giriniz")
+
+if(kullanıcıAd.includes(kullanıcıAdGir)){
+
+
+    alert("sisteme giriş yaptınız")
+
+
+}else{alert("Giriş yapamazsınız.")}
+
+   
+
+    */
+
+//6.soru
+
+/*let kazanc = 50; 
+let gun = "pazartesi";
+
+if(gün && kazanc){
+
+   alert(`kazanç: ${kazanc+=50}`) 
+}*/
+
+
+
+/*const tarayicilar = [
+    { name: "Google Chrome:", support: true},
+    { name: "Mozilla:", support: true},
+    { name: "Opera:", support: true},
+    { name: "Internet Explorer:", support: false}
+   ]
+
+   
+if(tarayicilar[0].name && tarayicilar[0].support==true){
+    
+
+    alert("sisteme giriş sağlandı")
+}*/
+   
+
+
+
+
+  // else if(tarayicilar[3].name || tarayicilar.support==false){
+  //  alert("Opera desteklemiyor giriş yapamazsınız")
+    //}
